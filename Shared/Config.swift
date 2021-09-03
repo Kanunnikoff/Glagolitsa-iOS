e7489ba669a8a66255e94a37496a4789787f0883
Канунниковъ Дмитрiй Сергѣевичъ <dmitry.kanunnikoff@gmail.com>
2021-09-03 23:02:22 +0300

    Некоторые доработки экрана с информацией о программе.

---
+++ Shared/Config.swift
@@ -0,0 +1,27 @@
+//
+//  Config.swift
+//  Config
+//
+//  Created by Kanunnikov Dmitriy Sergeevich on 03.09.2021.
+//
+
+struct Config {
+    
+    //--- Test
+    
+    static let isTestMode = true
+    
+    //--- App Store
+    
+    static let APPSTORE_APP_ID = 1545847004
+    static let APPSTORE_APP_URL = "https://itunes.apple.com/app/id\(APPSTORE_APP_ID)"
+    static let APPSTORE_APP_REVIEW_URL = "https://itunes.apple.com/app/id\(APPSTORE_APP_ID)?action=write-review"
+    static let APPSTORE_DEVELOPER_URL = "https://itunes.apple.com/developer/id1449411291"
+    
+    static let PACKAGE_NAME = "software.kanunnikoff.Koshka"
+    
+    //--- App
+    
+    static let PRIVACY_POLICY_URL = "https://ya.ru"
+    static let EMAIL_URL = "mailto:dmitry.kanunnikoff@gmail.com?subject=Koshka%20%28iOS%29"
+}
--- Shared/Views/AboutView.swift
+++ Shared/Views/AboutView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct AboutView: View {
+    
+    @State private var showingShareAlert = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                Section(header: Text("App Store")) {
+                    Link("Rate", destination: URL(string: Config.APPSTORE_APP_REVIEW_URL)!)
+                        .font(.system(size: 17, weight: .regular, design: .rounded))
+                    
+                    Button("Share") {
+                        showingShareAlert = true
+                    }
+                    .font(.system(size: 17, weight: .regular, design: .rounded))
+                    
+                    Link("Other apps", destination: URL(string: Config.APPSTORE_DEVELOPER_URL)!)
+                        .font(.system(size: 17, weight: .regular, design: .rounded))
+                }
+                
+                Section(header: Text("Feedback")) {
+                    Link("Write a letter", destination: URL(string: Config.EMAIL_URL)!)
+                        .font(.system(size: 17, weight: .regular, design: .rounded))
+                }
+                
+                Section(header: Text("Privacy Policy")) {
+                    Link("Read", destination: URL(string: Config.PRIVACY_POLICY_URL)!)
+                        .font(.system(size: 17, weight: .regular, design: .rounded))
+                }
+            }
+            .navigationTitle("About")
+            .sheet(isPresented: $showingShareAlert) {
+                ShareSheet(activityItems: [ Config.APPSTORE_APP_URL ])
+            }
+            
+            Spacer()
+            
+            Text("Version \(getAppVersion()), build \(getAppBuild())")
+                .font(.system(size: 10, weight: .regular, design: .rounded))
+        }
+    }
+    
+    private func getAppVersion() -> String {
+        guard let currentVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+            return ""
+        }
+        
+        return currentVersion
+    }
+    
+    private func getAppBuild() -> String {
+        let infoDictionaryKey = kCFBundleVersionKey as String
+        guard let build = Bundle.main.object(forInfoDictionaryKey: infoDictionaryKey) as? String else {
+            return ""
+            
+        }
+        
+        return build
     }
 }
 
@@ -18,3 +73,4 @@ struct AboutView_Previews: PreviewProvider {
         AboutView()
     }
 }
+
