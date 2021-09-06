0192cdee9c68f0e9de6d046e4a26c482b485a75c
Канунниковъ Дмитрiй Сергѣевичъ <dmitry.kanunnikoff@gmail.com>
2021-09-06 16:41:59 +0300

    Убраны лишние шрифты. Код для сохранения перевода в картинку.

--- iOS/Ext/ViewExt.swift
+++ iOS/Ext/ViewExt.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 // A View wrapper to make the modifier easier to use
 extension View {
+    
     func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
         self.modifier(DeviceRotationViewModifier(action: action))
+    }
+    
+    func snapshot() -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        let view = controller.view
+        
+        let targetSize = controller.view.intrinsicContentSize
+        view?.bounds = CGRect(origin: .zero, size: targetSize)
+        view?.backgroundColor = .clear
+        
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        
+        return renderer.image { _ in
+            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+        }
     }
 }
